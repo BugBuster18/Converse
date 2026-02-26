a6b534c745b1b74f3acf39a67188af68958f5a08
Venkatesh <dhangarvenkatesh52@gmail.com>
2026-02-27 01:20:28 +0530

    feat(auth):implement login and logout with JWT and cookie handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -45,9 +45,53 @@ export const signup = async (req,res)=>{
 
     }
 }
-export const login = (res,req)=>{
-    res.json("Signup")
+export const login = async (req,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where:{email}
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message:"invalid credentials"
+            })
+        }
+        else{
+            console.log(user)
+            const isPasswordCorrect = await bcrypt.compare(password,user.password)
+            if(!isPasswordCorrect){
+                 return res.status(400).json({
+                message:"invalid credentials"
+            })
+            }
+            generateToken(user.id,res)
+            res.status(200).json({
+                id:user.id,
+                fullName:user.fullName,
+                email:user.email,
+                profilePic:user.profilePic
+            })
+        }
+
+    } catch (error) {
+        console.log("error in login",error.message)
+        res.status(500).json({
+            message:"internal server error at login"
+        })
+    }
 }
 export const logout = (res,req)=>{
-    res.json("Signup")
+   try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({
+            message:"Logout successfully"
+        })
+    
+   } catch (error) {
+      console.log("error in logout",error.message)
+        res.json({
+            message:"internal server error"
+        }).status(500)
+   }
 } 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { login, logout, signup } from "../controllers/auth.controller.js"
 import { validate } from "../middleware/validate.js";
-import { signupSchema } from "../schema/auth.schema.js";
+import { loginSchema, signupSchema } from "../schema/auth.schema.js";
 
 const router = express.Router()
 
 router.post("/signup",validate(signupSchema), signup)
 
-router.post("/login", login)
+router.post("/login",validate(loginSchema), login)
 
 router.post("/logout", logout)
 
