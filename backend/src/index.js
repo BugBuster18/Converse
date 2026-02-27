e0e785680ac67b72829e53b9eab16534bf790a35
Venkatesh <dhangarvenkatesh52@gmail.com>
2026-02-28 03:01:03 +0530

    fix: fixed cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,15 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 
